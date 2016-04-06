a0849ea2b43dbac12fb364a853f0d9a06858b727
Kevin Korte <kevinkorte15@gmail.com>
2016-04-06 19:11:11 +0000

    changed email address

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,5 @@
 class UserNotifier < ApplicationMailer
-    default :from => 'weatheralert@titanhomesllc.com'
+    default :from => 'kevinkorte15@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(res)
