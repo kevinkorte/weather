2bd20d8f15e42dcb833ccc98277c4a89b1154f1e
Kevin Korte <kevinkorte15@gmail.com>
2016-12-13 13:33:25 -0800

    each weather event must be able to post the first time

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -62,42 +62,24 @@ scheduler.every '7m' do #23m
             else
                 puts "no need to save or alert on this event"
             end
+        else
+          Condition.create(
+              weather: res['current_observation']['weather'],
+              wind_string: res['current_observation']['wind_string'],
+              wind_dir: res['current_observation']['wind_dir'],
+              wind_mph: res['current_observation']['wind_mph'],
+              wind_gust_mph: res['current_observation']['wind_gust_mph'],
+              temp_f: res['current_observation']['temp_f'],
+              feelslike_f: res['current_observation']['feelslike_f'],
+              precip_today_in: res['current_observation']['precip_today_in'],
+              icon_url: res['current_observation']['icon_url'],
+              observation_time: res['current_observation']['observation_time'],
+              sent: "False",
+              forecast: "Current",
+              day_marker: "False",
+              isEmpty: "False"
+          )
         end
-
-        # adjusted_time = last_sent + 10.minutes # 6.hour
-        #     if adjusted_time < Time.now.utc
-        #     #night is today's date at 10pm UTC time
-        #     night = Time.new(Date.today.year, Date.today.month, Date.today.day, 22,0,0).utc
-        #     #morning is today's date at 6am UTC time
-        #     morning = Time.new(Date.today.year, Date.today.month, Date.today.day, 6,0,0).utc
-        #         if Time.now.utc > morning && Time.now.utc < night
-        #             weather = Condition.create(
-        #                 weather: res['current_observation']['weather'],
-        #                 wind_string: res['current_observation']['wind_string'],
-        #                 wind_dir: res['current_observation']['wind_dir'],
-        #                 wind_mph: res['current_observation']['wind_mph'],
-        #                 wind_gust_mph: res['current_observation']['wind_gust_mph'],
-        #                 temp_f: res['current_observation']['temp_f'],
-        #                 feelslike_f: res['current_observation']['feelslike_f'],
-        #                 precip_today_in: res['current_observation']['precip_today_in'],
-        #                 icon_url: res['current_observation']['icon_url'],
-        #                 observation_time: res['current_observation']['observation_time'],
-        #                 sent: "False",
-        #                 forecast: "Current",
-        #                 day_marker: "False",
-        #                 isEmpty: "False"
-        #             )
-        #             UserNotifier.send_current_text(weather).deliver_now
-        #             c = Condition.where(forecast: "Current").last
-        #             c.update(sent: "True")
-        #         end
-        #     end
-        # else
-        #     #This no longer works
-        #     UserNotifier.send_current_text(weather).deliver_now
-        #     c = Condition.where(forecast: "Current").last
-        #     c.update(sent: "True")
-        # end
     else
         puts "Current winds are not fast enough"
         puts Time.now
@@ -156,9 +138,26 @@ scheduler.every '2h' do
             UserNotifier.send_forecasted_text(@weather).deliver_now
             c = Condition.where(forecast: "Forecasted").last
             c.update(sent: "True")
+        else
+          @weather = Condition.create(
+                      weather: weather['condition'],
+                      wind_string: "Forecasted " + weather['wdir']['dir'] + " winds blowing " + weather['wspd']['english'] + " mph at " + weather['FCTTIME']['weekday_name'] + " " + weather['FCTTIME']['pretty'],
+                      wind_dir: weather['wdir']['dir'],
+                      wind_mph: weather['wspd']['english'],
+                      wind_gust_mph: "Unavailable",
+                      temp_f: weather['temp']['english'],
+                      feelslike_f: weather['feelslike']['english'],
+                      precip_today_in: "Unavailable",
+                      icon_url: weather['icon_url'],
+                      observation_time: weather['FCTTIME']['pretty'],
+                      sent: "False",
+                      forecast: "Forecasted",
+                      day_marker: "False",
+                      isEmpty: "False"
+          )
         end
-    end
-end
+    end #ends if we have something
+end #ends scheduler
 
 scheduler.every '30s' do
   res = HTTParty.get(ENV['ALERT_WEATHER_API']);
